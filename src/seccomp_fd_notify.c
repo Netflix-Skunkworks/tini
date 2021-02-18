30e92b7e243b0a505e0fe4cf272d7295293aeacd
Keerti Lakshminarayan <keerti@netflix.com>
2021-02-18 19:34:56 +0000

    Fixes after testing, added explicit sync using pthreads

--- src/seccomp_fd_notify.c
+++ src/seccomp_fd_notify.c
@@ -24,6 +24,9 @@
 #define PRINT_WARNING(...)  { fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); }
 #define PRINT_INFO(...)  { fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); }
 
+pthread_mutex_t wait_to_send = PTHREAD_MUTEX_INITIALIZER;
+int send_notify_fd = 0;
+pthread_cond_t ready_to_send = PTHREAD_COND_INITIALIZER;
 
 static int send_fd(int sock, int fd)
 {
@@ -101,14 +104,6 @@ struct sock_filter net_perf_filter[] = {
 		BPF_JUMP(BPF_JMP | BPF_JGE | BPF_K, X32_SYSCALL_BIT, 0, 1),
 		BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
 
-		/* Trap perf-related syscalls */
-		BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_bpf, 0, 1),
-		BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_USER_NOTIF),
-		BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_perf_event_open, 0, 1),
-		BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_USER_NOTIF),
-		BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_ioctl, 0, 1),
-		BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_USER_NOTIF),
-
 		/* Trap sendto */
 		BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_sendto, 0, 1),
 		BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_USER_NOTIF),
@@ -125,14 +120,16 @@ struct sock_filter net_perf_filter[] = {
 	};
 
 	char *is_handle_net = getenv("TITUS_SECCOMP_AGENT_HANDLE_NET_SYSCALLS");
-	if (strcmp(is_handle_net, "1") == 0) {
+	if (is_handle_net != NULL) {
 		prog.filter = net_perf_filter;
 		prog.len = 
 			(unsigned short)(sizeof(net_perf_filter) / sizeof(net_perf_filter[0]));
+		PRINT_INFO("Networking system calls will be intercepted");
 	} else {
 		prog.filter = perf_filter;
 		prog.len = 
 			(unsigned short)(sizeof(perf_filter) / sizeof(perf_filter[0]));
+		PRINT_INFO("BPF/Perf system calls will be intercepted");
 	}
 
 	/* Install the filter with the SECCOMP_FILTER_FLAG_NEW_LISTENER flag; as
@@ -146,6 +143,7 @@ struct sock_filter net_perf_filter[] = {
 		PRINT_WARNING("seccomp install_notify_filter failed: %s", strerror(errno));
 		return -1;
 	}
+	
 	return notify_fd;
 }
 
@@ -194,7 +192,7 @@ void * catch_send_fd(void *fd_arg)
 	}
 
 	resp = malloc(sizes.seccomp_notif_resp);
-	if (req == NULL) {
+	if (resp == NULL) {
 		PRINT_WARNING("malloc resp error!");
 		return NULL;
 	}
@@ -211,12 +209,16 @@ void * catch_send_fd(void *fd_arg)
 		PRINT_WARNING("Unexpected error in adding notify_fd to epoll watch");
 		return NULL;
 	}
+	PRINT_INFO("Listening to networking syscall notifications on %d..", notify_fd);
 
 	for (;;) {
 		/* Wait for next notification, returning info in '*req' */
 		memset(req, 0, sizeof(*req));
 		memset(resp, 0, sizeof(*resp));
-
+		pthread_mutex_lock(&wait_to_send);
+		send_notify_fd = 1;
+		pthread_cond_signal(&ready_to_send);
+		pthread_mutex_unlock(&wait_to_send);
 		ready = epoll_wait(epfd, evlist, MAX_EVENTS, -1);
 		if (ready == -1) {
 			if (errno == EINTR)
@@ -227,16 +229,20 @@ void * catch_send_fd(void *fd_arg)
 				return NULL;
 			}
 		}
+		PRINT_INFO("  fd=%d; events: %s%s%s", evlist[0].data.fd,
+			    (evlist[0].events & EPOLLIN) ? "EPOLLIN " : "",
+			    (evlist[0].events & EPOLLHUP) ? "EPOLLHUP " : "",
+			    (evlist[0].events & EPOLLERR) ? "EPOLLERR " : "");
 
 		if (evlist[0].events & EPOLLIN) {
 			if (ioctl(evlist[0].data.fd, SECCOMP_IOCTL_NOTIF_RECV,
 				  req) == -1) {
 				PRINT_WARNING(
-					"ioctlSECCOMP_IOCTL_NOTIF_RECV error");
+					"ioctl SECCOMP_IOCTL_NOTIF_RECV error");
 				return NULL;
 			}
 		}
-
+		
 		if (evlist[0].events & EPOLLHUP) {
 			/* Client has exited */
 			printf("Client termination notification");
@@ -262,6 +268,7 @@ void * catch_send_fd(void *fd_arg)
 			break;
 		}
 	}
+	PRINT_INFO("Stopped polling notifyfd = %d!", notify_fd);
 	return NULL;
 }
 
@@ -279,6 +286,7 @@ void maybe_setup_seccomp_notifer() {
 			return;
 		}
 
+		PRINT_INFO("Connecting to the tsa.sock");
 		struct sockaddr_un addr = {0};
 		addr.sun_family = AF_UNIX;
 		strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
@@ -295,9 +303,14 @@ void maybe_setup_seccomp_notifer() {
 		int notify_fd = -1;
 		notify_fd = install_notify_filter();
 
-		pthread_create(&thread_id, NULL, &catch_send_fd, (void *)&notify_fd);
-		pthread_detach(thread_id);
-	
+		pthread_create(&thread_id, NULL, &catch_send_fd, (void *)&notify_fd);		
+		pthread_mutex_lock(&wait_to_send);
+		while(send_notify_fd == 0) {
+			pthread_cond_wait(&ready_to_send, &wait_to_send);
+		}
+		PRINT_INFO("Can send the notify fd now!");
+		pthread_mutex_unlock(&wait_to_send);
+		
 
 		if (send_fd(sock_fd, notify_fd) == -1) {
 			PRINT_WARNING("Couldn't send fd to the socket at %s: %s", socket_path, strerror(errno));
@@ -305,6 +318,7 @@ void maybe_setup_seccomp_notifer() {
 		} else {
 			PRINT_INFO("Sent the notify fd to the seccomp agent socket at %s", socket_path)
 		}
+		pthread_join(thread_id, NULL);
 	}
 	return;
 }
